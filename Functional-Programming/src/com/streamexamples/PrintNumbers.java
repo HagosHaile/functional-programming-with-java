6ff8e3ebdd29719a3aa0ddd8ff614fc984a3cf59
Hagos Haile <hagosb.haile@gmail.com>
2022-04-03 12:27:31 -0500

    update to print cubes using maps

--- Functional-Programming/src/com/streamexamples/PrintNumbers.java
+++ Functional-Programming/src/com/streamexamples/PrintNumbers.java
@@ -1,39 +1,53 @@
 package com.streamexamples;
+
 import java.util.List;
 
 public class PrintNumbers {
 
 	public static void main(String[] args) {
-		List<Integer> numbers = List.of(12, 23, 2, 67, 23,12, 23, 2, 67, 23,100);
-		//printAllNumbers(numbers);
-		//printEvenNumbers(numbers);
-		printOddNumbers(numbers);
+		List<Integer> numbers = List.of(12, 23, 2, 67, 23, 12, 23, 2, 67, 23, 100);
+		// printAllNumbers(numbers);
+		// printEvenNumbers(numbers);
+		// printOddNumbers(numbers);
+		//printSquareOfEvenNumbers(numbers);
+		printSquareOfOddNumbers(numbers);
 	}
-
 
 	private static void printAllNumbers(List<Integer> numbers) {
-	
+
 		System.out.println("Print all numbers ");
-		numbers.stream()
-			.forEach(System.out::println);
+		numbers.stream().forEach(System.out::println);
 	}
-	
+
 	private static void printEvenNumbers(List<Integer> numbers) {
-		//add filter
-		//use filter(number->number%2==0) which is in lamdba expression instead of
-		//filter(PrintNumbers::isEven) which is method reference
+		// add filter
+		// use filter(number->number%2==0) which is in lamdba expression instead of
+		// filter(PrintNumbers::isEven) which is method reference
 		System.out.println("Print only Even numbers ");
-		numbers.stream()
-			.filter(number->number%2==0) //Lamdba Expression
-			.forEach(System.out::println);
-	}
-	
-	//Exercise 1
-	private static void printOddNumbers(List<Integer> numbers) {
-		numbers.stream()
-				.filter(num->num%2==1)
+		numbers.stream().filter(number -> number % 2 == 0) // Lamdba Expression
 				.forEach(System.out::println);
-		
+	}
+
+	// Exercise 1
+	private static void printOddNumbers(List<Integer> numbers) {
+		numbers.stream().filter(num -> num % 2 == 1).forEach(System.out::println);
+
+	}
+
+	private static void printSquareOfEvenNumbers(List<Integer> numbers) {
+
+		System.out.println("Print square of only Even numbers ");
+		numbers.stream().filter(number -> number % 2 == 0) // Lamdba Expression
+				.map(num -> num * num).forEach(System.out::println);
+	}
+
+	private static void printSquareOfOddNumbers(List<Integer> numbers) {
+
+		System.out.println("Print cube of only Odd numbers ");
+		numbers.stream()
+				.filter(number -> number % 2 == 0) // Lamdba Expression
+				.map(num -> num * num * num)
+				.forEach(System.out::println);
 	}
 
 }
